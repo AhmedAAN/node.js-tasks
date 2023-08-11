07d9ce70c25e562e7f800baaa70f0b829117e97c
AhmedAAN <ahmed.a.nagy@outlook.com>
2023-08-11 16:56:37 +0300

    javascript changed

--- Task02/background_color.js
+++ Task02/background_color.js
@@ -1,59 +1,81 @@
+//-------------Dark and Light Mode----------------
 const container = document.querySelector('.container')
 const main = document.querySelector('.main')
 const night = document.querySelector('.night')
 const day = document.querySelector('.day')
-function nightMode() {
-    container.classList.toggle("dark");
-    main.classList.toggle("dark");
-    night.classList.toggle("active");
+
+// Remove Light Mode
+function removeLight(){
     container.classList.remove("light");
     main.classList.remove("light");
     day.classList.remove("active");
-    body.style.background = ``
-    selector_l.value = '#ffffff'
-    selector_r.value = '#272727'
-    color.innerHTML = `(Dark Mode)`
 }
-function lightMode() {
-    container.classList.toggle("light");
-    main.classList.toggle("light");
-    day.classList.toggle("active");
+// Remove Dark Mode
+function removeDark() {
     container.classList.remove("dark");
     main.classList.remove("dark");
     night.classList.remove("active");
-    body.style.background = ``
-    selector_l.value = '#ffffff'
-    selector_r.value = '#272727'
-    color.innerHTML = `(Light Mode)`
+}
+
+// Toggle Dark Mode
+function nightMode() {
+    if (container.classList.contains("dark")) {
+        body.style.background = `linear-gradient(to bottom right, ${selector_l.value}, ${selector_r.value})`
+        color.innerHTML = `(to bottom right, ${selector_l.value}, ${selector_r.value})`
+        removeDark();
+    }
+    else {
+        body.style.background = ``
+        container.classList.add("dark");
+        main.classList.add("dark");
+        night.classList.add("active");
+        color.innerHTML = `(Dark Mode)`
+        removeLight();
+    }
+}
+
+// Toggle Light Mode
+function lightMode() {
+    if (container.classList.contains("light")) {
+        body.style.background = `linear-gradient(to bottom right, ${selector_l.value}, ${selector_r.value})`
+        color.innerHTML = `(to bottom right, ${selector_l.value}, ${selector_r.value})`
+        removeLight();
+    }
+    else {
+        body.style.background = ``
+        container.classList.add("light");
+        main.classList.add("light");
+        day.classList.add("active");
+        color.innerHTML = `(Light Mode)`
+        removeDark();
+    }
 }
 
 night.addEventListener('click', nightMode)
 day.addEventListener('click', lightMode)
 
 
+//-----------Background Color-----------------
 const selector_r = document.querySelector('.color-selector-r')
 const selector_l = document.querySelector('.color-selector-l')
 const body = document.querySelector('.container')
 const color = document.querySelector('.color')
 const random = document.querySelector('.random')
 color.innerHTML = `(to bottom right, ${selector_l.value}, ${selector_r.value})`
-function setBackgroundColor () {
-    container.classList.remove("light");
-    main.classList.remove("light");
-    container.classList.remove("dark");
-    main.classList.remove("dark");
-    night.classList.remove("active");
-    day.classList.remove("active");
+
+// Set Background Color from color selector
+function setBackgroundColor() {
+    removeLight();
+    removeDark();
+
     body.style.background = `linear-gradient(to bottom right, ${selector_l.value}, ${selector_r.value})`
     color.innerHTML = `(to bottom right, ${selector_l.value}, ${selector_r.value})`
 }
+
+// Create a Random Background
 function randomize() {
-    container.classList.remove("light");
-    main.classList.remove("light");
-    container.classList.remove("dark");
-    main.classList.remove("dark");
-    night.classList.remove("active");
-    day.classList.remove("active");
+    removeDark();
+    removeLight();
     const maxVal = 0xFFFFFF;
     let leftColor = '#' + Math.floor(Math.random() * maxVal).toString(16).padStart(6,0);
     let rightColor = '#' + Math.floor(Math.random() * maxVal).toString(16).padStart(6,0);
@@ -63,9 +85,9 @@ function randomize() {
     body.style.background = `linear-gradient(to bottom right, ${leftColor}, ${rightColor})`
     color.innerHTML = `(to bottom right, ${leftColor}, ${rightColor})`
 }
+
 selector_l.addEventListener('input', setBackgroundColor)
 selector_r.addEventListener('input', setBackgroundColor)
 random.addEventListener('click', randomize)
-
 
 
